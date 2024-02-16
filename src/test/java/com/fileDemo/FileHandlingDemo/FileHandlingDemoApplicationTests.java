84cdd4eaccf68f02aa1c281672f96317c427f902
koheinthurako <captainthura99@gmail.com>
2024-02-16 21:50:49 +0630

    start fileHandling

---
+++ src/test/java/com/fileDemo/FileHandlingDemo/FileHandlingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fileDemo.FileHandlingDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileHandlingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
